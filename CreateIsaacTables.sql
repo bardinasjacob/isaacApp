5bd0538c3d4691d2b94f94df55c59ce31683c2a8
Carcinophobic <bardinasjacob@gmail.com>
2024-10-29 12:21:45 -0400

    Adding character image files and adjusting db structure for soul hp

--- CreateIsaacTables.sql
+++ CreateIsaacTables.sql
@@ -4,14 +4,15 @@ DROP TABLE IF EXISTS character_table;
 CREATE table character_table(
     char_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     char_name VARCHAR(255), 
-    char_hp INT,
-    char_speed INT,
-    char_tears INT,
-    char_tears_mult INT,
-    char_dmg INT,
-    char_dmg_mult INT,
-    char_range INT,
-    char_shot_speed INT
+    char_red_hp INT,
+    char_soul_hp INT,
+    char_speed FLOAT,
+    char_tears FLOAT,
+    char_tears_mult FLOAT,
+    char_dmg FLOAT,
+    char_dmg_mult FLOAT,
+    char_range FLOAT,
+    char_shot_speed FLOAT
     );
 
 
@@ -19,12 +20,13 @@ DROP TABLE IF EXISTS item_table;
 CREATE table item_table(
     item_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     item_name VARCHAR(255), 
-    item_hp INT,
-    item_speed INT,
-    item_tears INT,
-    item_tears_mult INT,
-    item_dmg INT,
-    item_dmg_mult INT,
-    item_range INT,
-    item_shot_speed INT
+    item_red_hp INT,
+    item_soul_hp INT,
+    item_speed FLOAT,
+    item_tears FLOAT,
+    item_tears_mult FLOAT,
+    item_dmg FLOAT,
+    item_dmg_mult FLOAT,
+    item_range FLOAT,
+    item_shot_speed FLOAT
     );
--- InsertIsaacCharacters.sql
+++ InsertIsaacCharacters.sql
@@ -1,4 +1,31 @@
 USE isaac;
 
-INSERT INTO character_table (char_name, char_hp, char_speed, char_tears, char_dmg, char_dmg_mult, char_range, char_shot_speed) 
-VALUES ("Isaac", 3, 1, )
+INSERT INTO character_table (char_name, char_red_hp, char_soul_hp, char_speed, char_tears, char_dmg, char_dmg_mult, char_range, char_shot_speed) 
+VALUES ("Isaac", 3, 1, 1, 3, 1, 10, 5),
+("Maggie", 4, 0.6, 1, 3, 1, 10, 5),
+("Cain", 2, 1, 1, 4, 1, 10, 5),
+("Judas", 1, 1, 1, 7, 1, 10, 5),
+("Samson", 2, 1, 1, 4, 1, 10, 5),
+("Azazel", -3, 1, 1, 5, 1, 10, 5),
+("Eve", 2, 1, 1, 4, 1.5, 10, 5),
+("Bethany", 3, 1, 1, 3, 1, 10, 5),
+("Lilith", 1, 0.8, 1, 4, 1, 10, 5),
+("Lost", 1, 0.8, 1, 4, 1, 10, 5),
+("Forgotton", 1, 0.8, 1, 4, 1, 10, 5),
+("Keeper", 1, 0.8, 1, 4, 1, 10, 5),
+("Blue_Baby", 1, 0.8, 1, 4, 1, 10, 5),
+("Jacob_and_Esau", 1, 0.8, 1, 4, 1, 10, 5),
+("T_Isaac", 3, 1, 1, 3, 1, 10, 5),
+("T_Maggie", 4, 0.6, 1, 3, 1, 10, 5),
+("T_Cain", 2, 1, 1, 4, 1, 10, 5),
+("T_Judas", 1, 1, 1, 7, 1, 10, 5),
+("T_Samson", 2, 1, 1, 4, 1, 10, 5),
+("T_Azazel", 1, 1, 1, 5, 1, 10, 5),
+("T_Eve", 2, 1, 1, 4, 1.5, 10, 5),
+("T_Bethany", 3, 1, 1, 3, 1, 10, 5),
+("T_Lilith", 1, 0.8, 1, 4, 1, 10, 5);
+("T_Lost", 1, 0.8, 1, 4, 1, 10, 5),
+("T_Forgotton", 1, 0.8, 1, 4, 1, 10, 5),
+("T_Keeper", 1, 0.8, 1, 4, 1, 10, 5),
+("T_Blue_Baby", 1, 0.8, 1, 4, 1, 10, 5),
+("T_Jacob_and_Esau", 1, 0.8, 1, 4, 1, 10, 5),
