0e9ad276a32c2769efc2a7f945756b638c7a33c1
Carcinophobic <bardinasjacob@gmail.com>
2024-11-05 19:35:42 -0500

    Added item button scroll and updated button activation method to allow for more item names

---
+++ InsertIsaacItems.sql
@@ -0,0 +1,22 @@
+INSERT INTO item_table (
+    item_name, 
+    item_red_hp, 
+    item_soul_hp, 
+    item_speed, 
+    item_tears, 
+    item_tears_mult, 
+    item_dmg, 
+    item_dmg_mult, 
+    item_range, 
+    item_shot_speed, 
+    item_deal_rate
+) VALUES
+    ('Item Name 1', 1, 0, 1.0, 0.5, 1.0, 1.5, 1.0, 1.0, 1.2, 0.0),
+    ('Item Name 2', 0, 2, 0.8, 0.3, 1.2, 1.0, 1.1, 1.0, 1.0, 0.2),
+    ('Item Name 3', 1, 1, 1.1, 0.2, 1.5, 1.2, 1.3, 0.9, 1.1, 0.1),
+    ('Item Name 4', 1, 0, 1.0, 0.5, 1.0, 1.5, 1.0, 1.0, 1.2, 0.0),
+    ('Item Name 5', 0, 2, 0.8, 0.3, 1.2, 1.0, 1.1, 1.0, 1.0, 0.2),
+    ('Item Name 6', 1, 1, 1.1, 0.2, 1.5, 1.2, 1.3, 0.9, 1.1, 0.1),
+    ('Item Name 7', 1, 0, 1.0, 0.5, 1.0, 1.5, 1.0, 1.0, 1.2, 0.0),
+    ('Item Name 8', 0, 2, 0.8, 0.3, 1.2, 1.0, 1.1, 1.0, 1.0, 0.2),
+    ('Item Name 9', 1, 1, 1.1, 0.2, 1.5, 1.2, 1.3, 0.9, 1.1, 0.1);
